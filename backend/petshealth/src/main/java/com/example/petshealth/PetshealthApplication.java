5be3f89bda0d6f7180af1952847070717fc5da61
Filip Kramar <73394758+FilipKramar@users.noreply.github.com>
2024-05-18 19:36:53 +0200

    Initalised the start Spring Boot Application (#1)

---
+++ backend/petshealth/src/main/java/com/example/petshealth/PetshealthApplication.java
@@ -0,0 +1,13 @@
+package com.example.petshealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshealthApplication.class, args);
+	}
+
+}
---
+++ backend/petshealth/src/test/java/com/example/petshealth/PetshealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petshealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

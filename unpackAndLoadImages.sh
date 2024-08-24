5db83b02bdcead0c440ce6738bc20aa5fcc1d61d
Filip Kramar <73394758+FilipKramar@users.noreply.github.com>
2024-08-24 02:50:22 +0200

    Pethealth 24 (#14)
    * Update build-images.yaml
    
    * Update angular.json
    
    * Update build-images.yaml
    
    * Update build-images.yaml
    
    * Update build-images.yaml
    
    * Update build-images.yaml
    
    * Update build-images.yaml
    
    * Update Dockerfile
    
    * Update build-images.yaml
    
    * Update build-images.yaml
    
    * Update build-images.yaml
    
    * Update build-images.yaml
    
    * Update build-images.yaml
    
    * Update build-images.yaml
    
    * Update build-images.yaml
    
    * Update docker-compose.yml
    
    * Create unpackAndLoadImages.sh

---
+++ unpackAndLoadImages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# Check if unzip is installed, and install if not
+if ! command -v unzip &> /dev/null; then
+  echo "unzip not found, installing..."
+  sudo apt-get update && sudo apt-get install -y unzip
+fi
+
+ARTIFACT_ZIP="petshealth-docker-image.zip"
+IMAGE_ZIP="petshealth-image.zip"
+IMAGE_DIR="docker-images"
+
+unzip -q "$ARTIFACT_ZIP"
+unzip -q "$IMAGE_ZIP" -d "$IMAGE_DIR"
+
+for tar_file in "$IMAGE_DIR"/*.tar; do
+  docker load -i "$tar_file"
+done
+
+rm -rf "$IMAGE_DIR" "$IMAGE_ZIP"
+
+echo "Docker images loaded and cleaned up."
+
+docker-compose up -d
